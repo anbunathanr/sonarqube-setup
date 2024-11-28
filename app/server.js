b0fdb8b0e3f681d7698167ee71bb930a4ce2d8c1
anbunathan <anbunathan.r@sphinxworldbiz.co>
2024-11-28 16:00:22 +0530

    introduce one error
    let username = "admin";
    let sqlQuery = `SELECT * FROM users WHERE username = '${username}'`;

--- app/server.js
+++ app/server.js
@@ -3,6 +3,9 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   port = process.env.API_APP_ENV || 8888;
 
+let username = "admin";
+let sqlQuery = `SELECT * FROM users WHERE username = '${username}'`;
+
 const apiRoute = require('./controllers/api'),
   middlewares = require('./middleware/common-middleware');
 
